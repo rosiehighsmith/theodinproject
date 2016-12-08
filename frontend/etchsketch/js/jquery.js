97f166d75c22d958683cdb3f34ce7b34b3540a2a
Rosie <ariel.highsmith@gmail.com>
2016-12-07 16:40:57 -0800

    16 box grid and hover effect created

---
+++ frontend/etchsketch/js/jquery.js
@@ -0,0 +1,24 @@
+var rows = 16;
+var columns = 16;
+var $row = $("<div />", {
+    class: 'row'
+});
+var $square = $("<div />", {
+    class: 'square'
+});
+
+$(document).ready(function () {
+    //add columns to the the temp row object
+    for (var i = 0; i < columns; i++) {
+        $row.append($square.clone());
+    }
+    //clone the temp row object with the columns to the wrapper
+    for (var i = 0; i < rows; i++) {
+        $("#wrapper").append($row.clone());
+    }
+    $('.square').hover(function(){
+        $(this).css("background-color","yellow");
+        }, function() {
+        $(this).css("background-color","turquoise");
+    });
+});
